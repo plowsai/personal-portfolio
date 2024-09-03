ffa6c76fe4cc63debb2b28a7aabd0eeb4f88a11a
Jordan Plows <jordan@lightapi.com>
2024-09-02 17:06:47 -0700

    dev: janitor commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,12 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Jordan Plows Portfolio',
+    template: '%s | Jordan Plows Portfolio',
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Jordans Portfolio',
     description: 'This is my portfolio.',
     url: baseUrl,
     siteName: 'My Portfolio',
